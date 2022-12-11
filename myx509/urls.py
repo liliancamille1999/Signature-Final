e5f7fb1ffcf26f01afa0a62cfacc15b1c4cf31fa
liliancamille1999 <liliancamille1999@gmail.com>
2022-12-11 20:06:01 +0100

    4th

--- myx509/urls.py
+++ myx509/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from myx509.views import signer, DocList, verifList, PersonneList, \
+from myx509.views import signer, DocumentList,DocList, verifList, PersonneList, \
     CaList, EntrepriseList, verifDetail  # PersonneList, DocList, CaList,EntrepriseList
 
 router = routers.DefaultRouter()
 router.register('personne', PersonneList)
 router.register('cert', CaList)
 router.register('entreprise', EntrepriseList)
+router.register('document', DocumentList)
 
 urlpatterns = [
     path('signer', signer, name="signer"),
--- myx509/views.py
+++ myx509/views.py
@@ -51,7 +51,7 @@ class verifList(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
-        error=""
+    
         data = request.data
         # sign = '1Zazc8T+GBFCXMYq/VHMVETpsKGimbqY1Zb0yEzIZ7V78YwLDzCm/q8tahNm6XHPlJvWpoMB8T/QRynGTxN0AGLwZ9grVBUF75CcuzLr4nX5gEAgJqEHM+VWpWUltqxey0NSLl8HR2n0Ep/Ln2l0ec7MnFhXn9g30w0S9qc/XeYiyLOIhcZXr1ivQIIHagkXSfbIJL/mSWFsWjiCwXJp9iecJAylAsV5SrnGWewUJyfdqJYccIfL+Eh3f68JPy55YC/aXL/kya0bwG4OL3KhODGB7Qo1nrFRgrnlrE/ppTd+mu/5pxbkXJl4nJKsORtsHrA3zTl+5JixJQPYX7aB0Q=='
         sign = data['signature']
@@ -60,6 +60,7 @@ class verifList(APIView):
         try:
            signe4 = base64.b64decode(signe)
            fichier = DocSign.objects.filter(id=data['id_doc'])
+           print(fichier)
            fichier2 = DocSign.objects.get(id=data['id_doc'])
            fiche1 = fichier2.media
            doc = fiche1.read()
@@ -79,6 +80,10 @@ class PersonneList(viewsets.ModelViewSet):
     queryset = Personne.objects.all()
     serializer_class = PersonneSerializers
 
+class DocumentList(viewsets.ModelViewSet):
+    queryset = DocSign.objects.all()
+    serializer_class = DocSignSerializers
+
 
 class CaList(viewsets.ModelViewSet):
     queryset = Cert.objects.all()
@@ -91,6 +96,8 @@ class EntrepriseList(viewsets.ModelViewSet):
 
 
 def signer(request):
+    document_signe=0
+    document1=0
     id_u = request.user.id
     personne = Personne.objects.get(user_id=id_u)
     id_Per = personne.id
@@ -98,87 +105,86 @@ def signer(request):
     id_Cert = Certificat.id
     Entreprises_id = personne.Entreprise_id
     Entreprises = entreprise.objects.get(id=Entreprises_id)
-    date = datetime.now()
     if request.method == "POST":
-        media = request.FILES['media']
-        taille = media.size
-        if taille > 50000000:
-            messages.error(request, "votre fichier est trop volumineux")
-        else:
-            file = base64.b64encode(media.read())
-            passphrase = request.POST['mot_de_passe']
-
-            signe = DigitalSignature.Sign(DigitalSignature, id_u, passphrase, file)
-            if signe == 0:
-                Pdf = DocSign.objects.filter(personne=personne)
-                message = "votre document n'a pas ete signe"
+        medias = request.FILES.getlist('media')
+        for media in medias:
+            taille = media.size
+            date = datetime.now()
+            if taille > 50000000:
+                document_signe=document_signe+1
             else:
-                message = "votre document a ete signe"
-                DocSign.objects.create(media_nom=media.name, personne=personne, date_signature=date, media=media,
-                                       media_signe=media)
-                doc = DocSign.objects.latest("id")
-                id_doc = doc.id
-                signe2 = str(id_doc) + " " + str(id_Cert) + " "
-                signe3 = base64.b64encode(signe)
-                signe5 = bytes(signe2, encoding="utf-8")
-                signe6 = signe5 + signe3
-                signe4 = base64.b64decode(signe3)
+                file = base64.b64encode(media.read())
+                passphrase = request.POST['mot_de_passe']
 
-                print(signe3)
-                print(signe4)
-                print(signe)
-                print(signe2)
+                signe = DigitalSignature.Sign(DigitalSignature, id_u, passphrase, file)
+                if signe == 0:
+                    Pdf = DocSign.objects.filter(personne=personne)
+                    document_signe = document_signe + 1
+                else:
+                    document1=document1+1
+                    DocSign.objects.create(media_nom=media.name, personne=personne, date_signature=date, media=media,
+                                           media_signe=media)
+                    doc = DocSign.objects.latest("id")
+                    id_doc = doc.id
+                    signe2 = str(id_doc) + " " + str(id_Cert) + " "
+                    signe3 = base64.b64encode(signe)
+                    signe5 = bytes(signe2, encoding="utf-8")
+                    signe6 = signe5 + signe3
+                    signe4 = base64.b64decode(signe3)
 
-                # json.dumps(DATA)
-                img = qrcode.make(signe6)
+                    print(signe3)
+                    print(signe4)
+                    print(signe)
+                    print(signe2)
 
-                '''
-                qr = qrcode.QRCode(
-                    version=1,
-                    error_correction=qrcode.constants.ERROR_CORRECT_M,
-                    box_size=10,
-                    border=4,
-                )
-                print(type(qr))
-                qr0 = QrCode.encode_text("Bonjour, monde!", QrCode.Ecc.MEDIUM)
-                qr.add_data(signe2)
-                qr.add_data(signe3)
-                qr.make(fit=True)'''
+                    # json.dumps(DATA)
+                    img = qrcode.make(signe6)
 
-                # img = qr.make_image(fill_color="black", back_color="white")
-                c1 = canvas.Canvas('canvas.pdf')
-                file = img.get_image()
-                b = ImageReader(file)
-                # On insére l'image souhaitée dans le fichier précédent à la position souhaitée ('adresseimage.png',
-                # x, y)
-                c1.drawImage(b, 0, 0, width=87, height=87)
+                    '''
+                    qr = qrcode.QRCode(
+                        version=1,
+                        error_correction=qrcode.constants.ERROR_CORRECT_M,
+                        box_size=10,
+                        border=4,
+                    )
+                    print(type(qr))
+                    qr0 = QrCode.encode_text("Bonjour, monde!", QrCode.Ecc.MEDIUM)
+                    qr.add_data(signe2)
+                    qr.add_data(signe3)
+                    qr.make(fit=True)'''
 
-                c1.save()
+                    # img = qr.make_image(fill_color="black", back_color="white")
+                    c1 = canvas.Canvas('canvas.pdf')
+                    file = img.get_image()
+                    b = ImageReader(file)
+                    # On insére l'image souhaitée dans le fichier précédent à la position souhaitée ('adresseimage.png',
+                    # x, y)
+                    c1.drawImage(b, 0, 0, width=87, height=87)
 
-                # On ouvre le fichier contenant le tamon
-                montampon = PdfFileReader(open("canvas.pdf", "rb"))
+                    c1.save()
 
-                # On ouvre le fichier sur lequel le tampon doit être inséré
-                output_file = PdfFileWriter()
-                fichier = DocSign.objects.latest('id')
-                fiche = fichier.media_signe
-                fiche1 = fichier.media
-                input_file = PdfFileReader(open(fiche1.path, "rb"))
+                    # On ouvre le fichier contenant le tamon
+                    montampon = PdfFileReader(open("canvas.pdf", "rb"))
 
-                # On récupère le nombre de pages du fichier
-                page_count = input_file.getNumPages()
+                    # On ouvre le fichier sur lequel le tampon doit être inséré
+                    output_file = PdfFileWriter()
+                    fichier = DocSign.objects.latest('id')
+                    fiche = fichier.media_signe
+                    fiche1 = fichier.media
+                    input_file = PdfFileReader(open(fiche1.path, "rb"))
 
-                # On ajoute à chaque page le tampon avec une boucle pour
-                for page_number in range(page_count):
-                    input_page = input_file.getPage(page_number)
-                    input_page.mergePage(montampon.getPage(0))
-                    output_file.addPage(input_page)
+                    # On récupère le nombre de pages du fichier
+                    page_count = input_file.getNumPages()
 
-                with open(fiche.path, "wb") as OutputStream:
-                    output_file.write(OutputStream)
+                    # On ajoute à chaque page le tampon avec une boucle pour
+                    for page_number in range(page_count):
+                        input_page = input_file.getPage(page_number)
+                        input_page.mergePage(montampon.getPage(0))
+                        output_file.addPage(input_page)
 
-                Pdf = DocSign.objects.filter(personne=personne)
-                # test1=createVerif(createVerif,21,1)
-                # print(test1)
+                    with open(fiche.path, "wb") as OutputStream:
+                        output_file.write(OutputStream)
+    Pdf = DocSign.objects.filter(personne=personne)
+    message=str(document1)+" documents signes et "+str(document_signe)+" documents non signes"
     return render(request, 'Accueil.html',
                   {'message': message, 'Document': Pdf, 'Personne': personne, 'entreprise': Entreprises})
